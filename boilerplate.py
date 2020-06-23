797187c1962238a57fd3b1bf1edec6e3f4562ebe
Matthew <mattyg@live.unc.edu>
2020-06-22 22:12:44 -0400

    Brought boilerplate up-to-date with Qiskit 0.19.5

--- boilerplate.py
+++ boilerplate.py
@@ -1,6 +1,3 @@
-# Boilerplate code to interface with Qiskit
-# by Matthew Gregoire, 2019
-
 # In general, not all imports are needed. Important ones include:
 # IBMQ, QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
 
@@ -8,26 +5,24 @@ from qiskit import *
 from qiskit.visualization import plot_histogram
 from qiskit.providers.ibmq import least_busy
 
-Q = 3   # number of qubits needed for the circuit
+Q = 3   # number of qubits
 C = 3   # number of classical bits
 
-# Requires at least one account to already be saved.
-# If not, consult the Qiskit documentation at qiskit.org.
-IBMQ.load_account()
-provider = IBMQ.get_provider(hub='ibm-q')
+provider = IBMQ.load_account()
 
+# Quantum circuit goes here. For reference, this creates a three-qubit Bell state.
 qc = QuantumCircuit(Q,C)
 
-# Quantum circuit goes here
-
 qc.h(0)
-qc.cx(0,1)
-qc.measure([0,1], [0,1])
+qc.cx(0, 1)
+qc.cx(0,2)
+qc.measure([0,1,2], [0,1,2])
 
-# Draw the quantum circuit. 
-# qc.draw(output='mpl').savefig("path\\to\\__circuit_file___.png")
+# Draw the quantum circuit and output it to a file. 
+# qc.draw(output='mpl').savefig("C:\\Users\\matth\\Documents\\Documents\\Qiskit\\__circuit_file___.png")
 
-# Number of times to run the circuit
+print(qc.draw(output="text"))
+
 SHOTS = 1024
 
 # Run the job on a simulator
@@ -37,22 +32,23 @@ counts = sim_job.result().get_counts(qc)
 print("Simulator results:", counts)
 
 # Run the job on a real quantum computer
-# Some example filters to select a backend are shown.
-backend = least_busy(provider.backends(simulator=False, filters=lambda x: x.configuration().n_qubits >= Q and x.name() != 'ibmq_16_melbourne'))
+backend = least_busy(provider.backends(simulator=False))
 print("The least busy backend is ", backend)
 job = execute(qc, backend, shots=SHOTS)
 real_counts = job.result().get_counts(qc)
 print("Actual result:", real_counts)
-# Retrieve an already existing job on a certain backend
-# Change NAME and JOB_ID to access the correct job
-NAME = 'ibmqx4'
-JOB_ID = ''
-backend = IBMQ.get_backend(NAME)
-backend.retrieve_job(JOB_ID)
-old_counts = job.result().get_counts(qc)
-print("Old result:", old_counts)
 
-# Plot the results of the computation as a histogram.
-# ***change the file name each time***
-# Still a work in progress to get the formatting correct!
-plot_histogram([counts, real_counts], legend=['Simulated', 'Actual'], title='Experiment', figsize=(9,5)).savefig("path\\to\\__result_file__.png")
+# # Retrieve an already existing job on a certain backend
+# # Change NAME and JOB_ID to access the correct job
+# NAME = 'ibmqx4'
+# JOB_ID = ''
+# backend = provider.get_backend(NAME)
+# backend.retrieve_job(JOB_ID)
+# old_counts = job.result().get_counts(qc)
+# print("Old result:", old_counts)
+
+# # Plot the results of the computation as a histogram.
+# # ***change the file name each time***
+# # Still a work in progress to get the formatting correct!
+
+# plot_histogram([counts, real_counts], legend=['Simulated', 'Actual'], title='Experiment', figsize=(9,5)).savefig("C:\\Users\\matth\\Documents\\Documents\\Qiskit\\__result_file__.png")
